de88b99d3f79cfd2c41b79f25710eb3d802fecb6
Keith Fuller <keith66fuller@gmail.com>
2018-07-02 11:50:31 -0500

    pretty much done.  need to check the instructions.

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,113 +1,127 @@
 
-$( document ).ready(() => {
+$(document).ready(() => {
+  // A way of populating the questions that better lends itself to updating the questions.
+  [
+    "It's my way or the highway.",
+    "It's important to never make any mistakes whatsoever.",
+    "Questions of morality ultimately boil down to whatever benefits me the most at the time.",
+    "You rarely apologize.",
+    "People rarely rise to my standards of good conduct.",
+    "Pepsi is better than Coke.",
+    "To the victor go the spoils.",
+    "The apple doesn't fall far from the tree.",
+    "There is nothing more deceptive than an obvious fact. (Arthur Conan Doyle)",
+    "Energetic people frighten you."
+    ].forEach((question,idx) => {
+    let select = $(`<select class="chosen-select" id="q${idx+1}">`)
+    $(select).append(`<option value=""></option>`);
+    $(select).append(`<option value="${1}">${1} (Strongly Disagree)</option>`);
+    $(select).append(`<option value="${2}">${2}</option>`);
+    $(select).append(`<option value="${3}">${3}</option>`);
+    $(select).append(`<option value="${4}">${4}</option>`);
+    $(select).append(`<option value="${5}">${5} (Strongly Agree)</option>`);
+    $('#questionsGoHere').append($('<h3>').append(`<strong>Question ${idx+1}</strong>`));
+    $('#questionsGoHere').append(`<h4>${question}</h4>`);
+    $('#questionsGoHere').append($(select));
+  });
+   
+  
+  var config = {
+    ".chosen-select": {},
+    ".chosen-select-deselect": {
+      allow_single_deselect: true
+    },
+    ".chosen-select-no-single": {
+      disable_search_threshold: 10
+    },
+    ".chosen-select-no-results": {
+      no_results_text: "Oops, nothing found!"
+    },
+    ".chosen-select-width": {
+      width: "95%"
+    }
+  };
 
-  function rNum(start,end) {
-    return Math.floor(Math.random()*(end-start)+start)
+  for (var selector in config) {
+    $(selector).chosen(config[selector]);
   }
-    var config = {
-        ".chosen-select": {},
-        ".chosen-select-deselect": {
-          allow_single_deselect: true
-        },
-        ".chosen-select-no-single": {
-          disable_search_threshold: 10
-        },
-        ".chosen-select-no-results": {
-          no_results_text: "Oops, nothing found!"
-        },
-        ".chosen-select-width": {
-          width: "95%"
+
+  // Capture the form inputs
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    // Form validation
+    function validateForm() {
+      var isValid = true;
+      $(".form-control").each(function () {
+        if ($(this).val() === "") {
+          isValid = false;
         }
+      });
+
+      $(".chosen-select").each(function () {
+
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+      return isValid;
+    }
+
+    // If all required fields are filled
+    if (validateForm()) {
+      // Create an object for the user"s data
+      var userData = {
+        name: $("#name").val(),
+        photo: $("#photo").val(),
+        scores: [
+          $("#q1").val(),
+          $("#q2").val(),
+          $("#q3").val(),
+          $("#q4").val(),
+          $("#q5").val(),
+          $("#q6").val(),
+          $("#q7").val(),
+          $("#q8").val(),
+          $("#q9").val(),
+          $("#q10").val()
+        ]
       };
-  
-      for (var selector in config) {
-        $(selector).chosen(config[selector]);
-      }
-  
-      // Capture the form inputs
-      $("#submit").on("click", function (event) {
-        event.preventDefault();
-  
-        // Form validation
-        function validateForm() {
-          var isValid = true;
-          $(".form-control").each(function () {
-            if ($(this).val() === "") {
-              isValid = false;
-            }
-          });
-  
-          $(".chosen-select").each(function () {
-  
-            if ($(this).val() === "") {
-              isValid = false;
-            }
-          });
-          return isValid;
-        }
-  
-        // If all required fields are filled
-        if (validateForm()) {
-          // Create an object for the user"s data
-          var userData = {
-            name: $("#name").val(),
-            photo: $("#photo").val(),
-            scores: [
-              $("#q1").val(),
-              $("#q2").val(),
-              $("#q3").val(),
-              $("#q4").val(),
-              $("#q5").val(),
-              $("#q6").val(),
-              $("#q7").val(),
-              $("#q8").val(),
-              $("#q9").val(),
-              $("#q10").val()
-            ]
-          };
-  
-          // AJAX post the data to the friends API.
-          $.post("/api/friends", userData, function (data) {
-  
-            // Grab the result from the AJAX post so that the best match's name and photo are displayed.
-            $("#match-name").text(data.name);
-            $("#match-img").attr("src", data.photo);
-  
-            // Show the modal with the best match
-            $("#results-modal").modal("toggle");
-  
-          });
-        } else {
-          alert("Please fill out all fields before submitting!");
-        }
+
+      // AJAX post the data to the friends API.
+      $.post("/api/friends", userData, function (data) {
+
+        // Grab the result from the AJAX post so that the best match's name and photo are displayed.
+        $("#match-name").text(data.name);
+        $("#match-img").attr("src", data.photo);
+
+        // Show the modal with the best match
+        $("#results-modal").modal("toggle");
+
       });
-      $("#randomize").on("click", function (event) {
-        event.preventDefault();
-          // AJAX post the data to the friends API.
-          $.post("/api/friends", {
-            name: "Joe Sixpack",
-            photo: "../images/random.jpg",
-            scores: [
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5),
-              rNum(1,5)
-            ]
-          }, function (data) {
-  
-            // Grab the result from the AJAX post so that the best match's name and photo are displayed.
-            $("#match-name").text(data.name);
-            $("#match-img").attr("src", data.photo);
-  
-            // Show the modal with the best match
-            $("#results-modal").modal("toggle");
-  
-          });
-      });
+    } else {
+      alert("Please fill out all fields before submitting!");
+    }
+  });
+  $("#randomize").on("click", function (event) {
+    event.preventDefault();
+    // Make a user with random scores for the lazy user.
+    const randomMe = {
+      name: "Joe Sixpack",
+      photo: "../images/random.jpg",
+      scores: []
+    };
+    for (let i = 0; i < 10; i++) { randomMe.scores.push(Math.floor(Math.random() * (4) + 1)) }
+
+    // Populate the inputs with the fake info.
+    $("#name").val(randomMe.name).trigger("chosen:updated");
+    $("#photo").val(randomMe.photo).trigger("chosen:updated");
+
+    randomMe.scores.forEach((score, idx) => {
+      $(`#q${idx + 1}`).val(score).trigger("chosen:updated");
+    });
+
+    // Trigger submit just as in non-random case.
+    $("#submit").trigger("click");
+  });
 })
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,30 @@
-var friends = require("../data/friends");
+const friends = require("../data/friends");
 
 function userDiff(a,b) {
-  console.log(`A.SCORES: ${a.scores}`)
-  console.log(`B.SCORES: ${b.scores}`)
-  console.log(`REDUCE: ${a.scores.reduce((acc, curr, idx) => {
-    return acc + Math.abs(curr - b.scores[idx])
-  }, 0)}`)
+  'use strict';
+
   return a.scores.reduce((acc, curr, idx) => {
-    return acc + Math.abs(curr - b.scores[idx])
-  }, 0)
+    return acc + Math.abs(curr - b.scores[idx]);
+  }, 0);
 }
 
-function diffSort(a,b) {
-  return a.diff - b.diff
-}
-
-module.exports = function(app) {
+module.exports = function(app) { ;
+  "use strict";
 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
-    //Someone wants to take the survey; let us find a match.
-    console.log(`INCOMING: ${JSON.stringify(req.body)}`)
-    const user = req.body;
-
-    let bestScore = friends.map((friend, i) => { return {'diff': userDiff(user,friend), 'index': i}} ).sort((a,b) => a.diff - b.diff)[0].index
-    console.log(`DIFF: ${userDiff(user,friends[0])}`)
-    console.log(scores)
-    console.log(bestScore)
-
-
+  app.post("/api/friends", function (req, res) {
+    res.send(
+      friends[
+      friends.map((friend, i) => {
+        return {
+          'diff': userDiff(req.body, friend),
+          'index': i
+        };
+      }).sort((a, b) => a.diff - b.diff)[0].index
+      ]
+    );
   });
 };
